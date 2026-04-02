0326c783575f478df16877f96e1c72ffac8981d1
Derrick01-AGM <3474232842@qq.com>
2026-04-02 21:14:11 +0800

    二段跳
    jump

--- My project/Assets/chicken/jump.cs
+++ My project/Assets/chicken/jump.cs
@@ -1,22 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour
+public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float jumpForce = 7f;
+    public int maxJumps = 2;
+    private int jumpsLeft;
+    private Rigidbody2D rb;
+    private bool hasResetBelow = false;  // 是否已经在下方重置过
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        jumpsLeft = maxJumps;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space"))
+        // 当角色 Y 坐标低于 -3.5 且还未重置过时，重置跳跃次数
+        if (transform.position.y <= -3.5f && !hasResetBelow)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(5,7,0);
-            
+            jumpsLeft = maxJumps;
+            hasResetBelow = true;  // 标记已重置，避免每帧重置
+        }
+
+        // 如果角色重新上升到 -3.5 以上，重置标志，允许下次下落时再次重置
+        if (transform.position.y > -3.5f)
+        {
+            hasResetBelow = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpsLeft--;
         }
     }
 }
